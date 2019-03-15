ab3cb4ad927ddafc4c3c0b71ab989e91d35740f0
SEAN HAMILTON <sean.hamilton@aot-technologies.com>
2019-03-15 12:10:23 -0700

    added interfaces for transformations

---
+++ src/app/core/interfaces/i-http-data.ts
@@ -0,0 +1,56 @@
+export interface IOrgInformationDef {
+  org_name: string;
+  org_num: string;
+  suite_num: string;
+  street_num: string;
+  address_2: string;
+  city: string;
+  province: string;
+  postal_code: string;
+  contracting_out: IContractingOut;
+}
+
+export interface IContractingOut {
+  contracting_third_party: YesNo;
+  third_party_org_num?: string;
+}
+
+export interface ICoreUserDef {
+  sa_curtesy_title?: string;
+  sa_last_name: string;
+  sa_intial?: string;
+  sa_job_title: string;
+  sa_email: string;
+  sa_phone_num: string;
+  sa_phone_ext?: string;
+  sa_fax_num?: string;
+}
+
+export interface ISigningAuthorityInformationDef extends ICoreUserDef {
+  sa_msp_access: YesNo;
+  sa_spg: string;
+}
+
+export interface IAccessAdministratorPresentDef extends ICoreUserDef {
+  aa_msp_access: YesNo;
+  aa_spg: string;
+}
+
+export interface IUserDef extends ICoreUserDef {
+  user_spg: string;
+}
+
+export interface IMspGroupDef {
+  mspgroup_num: string;
+  third_party: YesNo;
+}
+
+export interface ISiteregRequest {
+  org_information: IOrgInformationDef;
+  signing_authority_information: ISigningAuthorityInformationDef;
+  access_administrator_present: IAccessAdministratorPresentDef[];
+  users: IUserDef;
+  msp_group: IMspGroupDef;
+}
+
+export type YesNo = 'yes' | 'no';
--- src/app/modules/msp-register/services/msp-register-data.service.spec.ts
+++ src/app/modules/msp-register/services/msp-register-data.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MspRegisterDataService } from './msp-register-data.service';
+import { organization } from '@msp-register/mocks';
 
 describe('MspRegisterDataService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
@@ -8,5 +9,47 @@ describe('MspRegisterDataService', () => {
   it('should be created', () => {
     const service: MspRegisterDataService = TestBed.get(MspRegisterDataService);
     expect(service).toBeTruthy();
+  });
+
+  it('should return a valid IOrganizationDef', () => {
+    const org = organization;
+    const service: MspRegisterDataService = TestBed.get(MspRegisterDataService);
+    expect(service.mapOrgInformation(org)).toBeDefined();
+    // expect(service.mapOrgInformation(org)).toContain('org_name');
+  });
+
+  it('should return a valid ISigningAuthorityDef', () => {
+    const org = organization;
+    const service: MspRegisterDataService = TestBed.get(MspRegisterDataService);
+    expect(service.mapOrgInformation(org)).toBeDefined();
+    // expect(service.mapOrgInformation(org)).toContain('org_name');
+  });
+
+  it('should return a valid IUserDef', () => {
+    const org = organization;
+    const service: MspRegisterDataService = TestBed.get(MspRegisterDataService);
+    expect(service.mapOrgInformation(org)).toBeDefined();
+    // expect(service.mapOrgInformation(org)).toContain('org_name');
+  });
+
+  it('should return a valid IAccessAdministratorPresentDef', () => {
+    const org = organization;
+    const service: MspRegisterDataService = TestBed.get(MspRegisterDataService);
+    expect(service.mapOrgInformation(org)).toBeDefined();
+    // expect(service.mapOrgInformation(org)).toContain('org_name');
+  });
+
+  it('should return a valid IMspGroupDef', () => {
+    const org = organization;
+    const service: MspRegisterDataService = TestBed.get(MspRegisterDataService);
+    expect(service.mapOrgInformation(org)).toBeDefined();
+    // expect(service.mapOrgInformation(org)).toContain('org_name');
+  });
+
+  it('should return a valid ISiteRegRequest', () => {
+    const org = organization;
+    const service: MspRegisterDataService = TestBed.get(MspRegisterDataService);
+    expect(service.mapOrgInformation(org)).toBeDefined();
+    // expect(service.mapOrgInformation(org)).toContain('org_name');
   });
 });
