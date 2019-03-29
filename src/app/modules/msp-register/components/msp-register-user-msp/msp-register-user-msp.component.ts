ed8e676d31d3d804258abd65870673761b6c3e79
Faheem <faheemfactor@gmail.com>
2019-03-29 09:32:52 -0700

    user-msp  changed parent component

--- src/app/modules/msp-register/components/msp-register-user-msp/msp-register-user-msp.component.ts
+++ src/app/modules/msp-register/components/msp-register-user-msp/msp-register-user-msp.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
-import { BehaviorSubject } from 'rxjs';
-import { MspRegisterStateService } from '@msp-register/services/msp-register-state.service';
 import { Router } from '@angular/router';
-import { MspRegisterDataService } from '@msp-register/services/msp-register-data.service';
 import {
     validFormControl,
     validMultiFormControl,
@@ -18,17 +15,9 @@ export class MspRegisterUserMspComponent implements OnInit {
     @Input() fg: FormGroup;
     validFormControl: () => boolean;
 
-    constructor(
-        private router: Router
-    ) {
-        // public mspRegDataSvc: MspRegisterDataService
-        // public mspRegisterStateSvc: MspRegisterStateService,
-
-        // this.fg = this.mspRegisterStateSvc.mspRegisterUserMspForm;
-
-       this.validFormControl = validFormControl.bind(this);
-       // this.validFormControl = validMultiFormControl.bind(this);
-        // this.fg.valueChanges.subscribe((obs) => console.log(this.fg));
+    constructor(private router: Router) {
+        this.validFormControl = validFormControl.bind(this);
+        // this.validFormControl = validMultiFormControl.bind(this);
     }
 
     ngOnInit() {}
