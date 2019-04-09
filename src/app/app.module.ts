cb8dafea0f8f042c0c3c8ab029dd5ff064bb1a1d
Jam <jam.hamidi@maximusbc.ca>
2019-04-09 16:37:22 -0700

    testing logging

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,12 +9,11 @@ import { MspRegisterModule } from './modules/msp-register/msp-register.module';
 import { SharedCoreModule } from 'moh-common-lib';
 import { SharedModule } from '@shared/shared.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { Logger } from './core/services/logger.service';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, CoreModule, SharedModule],
-    providers: [Logger],
+    imports: [BrowserModule, AppRoutingModule, HttpClientModule, CoreModule, SharedModule],
+    providers: [],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
