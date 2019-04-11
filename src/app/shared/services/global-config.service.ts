fbaf17c904c5f0a5b3b8f0b86f27e52dd1bedefa
Faheem <faheemfactor@gmail.com>
2019-04-11 13:12:27 -0700

    debug - verifying log

--- src/app/shared/services/global-config.service.ts
+++ src/app/shared/services/global-config.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { UUID } from 'angular2-uuid';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,30 @@ export class GlobalConfigService {
   public get currentEnironment() {
     return environment;
   }
+
+
+  //#region msp ApplicationId specific
+
+  private mspApplicationId: string;
+
+  public logRefreshMspApplicationUUID(): string {
+    this.mspApplicationId = UUID.UUID();
+    return this.mspApplicationId;
+  }
+
+  /**
+   * Application UUID refers to Application of MSP, means the each application user apply will have a specific uuid
+   */
+  public get logMspApplicationUUID() {
+    return this.mspApplicationId ? this.mspApplicationId : this.logRefreshMspApplicationUUID();
+  }
+
+
+  public get logMspApplicationName() {
+    return 'sitereg';
+  }
+
+  //#endregion
+
 }
 
