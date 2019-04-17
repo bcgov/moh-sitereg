0e30b37e19639b4544209fc18d913f8c09f795e7
Faheem <faheemfactor@gmail.com>
2019-04-17 09:04:43 -0700

    debug code removed

--- src/app/modules/msp-register/models/msp-register-signing-authority.ts
+++ src/app/modules/msp-register/models/msp-register-signing-authority.ts
@@ -21,33 +21,18 @@ import {
 export class MspRegisterSigningAuthority
     extends GenerateForm<IMspSigningAuthority>
     implements IMspSigningAuthority {
-    // userTitle?: ctFormControlUserTitle = null;
-    // firstName: ctFormControlString = null;
-    // initial?: ctFormControlString = null;
-    // lastName: ctFormControlString = null;
-    // jobTitle: ctFormControlString = null;
-    // emailAddress: ctFormControlString = null;
-    // confirmEmail: ctFormControlString = null;
-    // phone: ctFormControlString = null;
-    // ext?: ctFormControlString = null;
-    // fax: ctFormControlString = null;
-    // administeringFor: ctFormControlString = null;
-    // directMspAccess: ctFormControlBoolean = false;
-
-    userTitle = null;
-    firstName = 'Faheem';
-    initial = 'A';
-    lastName = 'Wattoo';
-    jobTitle = 'Angular Developer';
-    emailAddress = 'faheemfactor@gmail.com';
-    confirmEmail = 'faheemfactor@gmail.com';
-    phone = '1234567890';
-    ext = 'A';
-    fax = '1234567890';
-    administeringFor = 'Employees';
-    directMspAccess = false;
-
-    //#endregion
+    userTitle?: ctFormControlUserTitle = null;
+    firstName: ctFormControlString = null;
+    initial?: ctFormControlString = null;
+    lastName: ctFormControlString = null;
+    jobTitle: ctFormControlString = null;
+    emailAddress: ctFormControlString = null;
+    confirmEmail: ctFormControlString = null;
+    phone: ctFormControlString = null;
+    ext?: ctFormControlString = null;
+    fax: ctFormControlString = null;
+    administeringFor: ctFormControlString = null;
+    directMspAccess: ctFormControlBoolean = false;
 
     get validators() {
         return cUserMspValidators;
