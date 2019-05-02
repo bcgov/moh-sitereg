585d5df29c7b8f10a420dfbf1ae45c94100e08fe
Faheem <faheemfactor@gmail.com>
2019-05-02 07:37:29 -0700

    fix: IE Compatibility Issues

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -51,12 +51,42 @@
  *  (window as any).__Zone_enable_cross_context_check = true;
  *
  */
+import 'classlist.js';
+/***************************************************************************************************
+ * APPLICATION IMPORTS
+ */
+
+/***************************************************************************************************
+* BROWSER POLYFILLS
+*/
+
+/**
+ *  IE9, IE10 and IE11 requires all of the following polyfills.
+ */
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/function';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/number';
+import 'core-js/es6/math';
+import 'core-js/es6/string';
+import 'core-js/es6/date';
+import 'core-js/es6/array';
+import 'core-js/es6/regexp';
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+
+import 'core-js/es7/object';
+
+/*
+ Evergreen browsers require these. 
+ */
+import 'core-js/es6/reflect';
+import 'core-js/es7/reflect';
+
 
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone'; // Included with Angular CLI.
-
-/***************************************************************************************************
- * APPLICATION IMPORTS
- */
