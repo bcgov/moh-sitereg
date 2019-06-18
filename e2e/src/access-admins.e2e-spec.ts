68eabe9e0903630af2605cd026aac56b4fbd796d
cyril bravo <cyril.bravo@vs-dev015.maximusbchealth.local>
2019-06-18 18:24:16 -0400

    Rename npm script for happy-path

--- e2e/src/access-admins.e2e-spec.ts
+++ e2e/src/access-admins.e2e-spec.ts
@@ -4,109 +4,106 @@ import { AccessAdminsPage } from './sitereg.po';
 import { JSONDataSiteReg } from '../../e2e/run-with-data.js';
 
 describe('Moh SiteReg - Access Admins Page', () => {
-    let aaPage: AccessAdminsPage;
-    let data = new FakeDataSiteReg();
-    let aaData, aaData2;
-    const AA_PAGE_URL = `register/access-admins`;
+    let accessPage: AccessAdminsPage;
+    const data = new FakeDataSiteReg();
+    let accessData;
+    const ACCESS_PAGE_URL = `register/access-admins`;
     const USERS_PAGE_URL = `register/users`;
-
     const jsonData = data.getJSONData();
-    if (data != null) { // Uses JSON data
-        data = jsonData;
+    if (data.hasJsonData) { // Uses JSON data
+        accessData = jsonData;
     }
 
     beforeAll(() => {
-        console.log('START OF E2E ENROLMENT' + '\nThis test uses Seed #: ' + data.getSeed());
+        if (data.hasJsonData === false) {
+            console.log('START OF MOH-SITEREG E2E' + '\nThis test uses Seed #: ' + data.getSeed());
+        }
     });
 
     beforeEach(() => {
-        aaPage = new AccessAdminsPage();
-        aaData = data.signingAuthorityInfo();
-        if (data.hasJsonData == false) {
-            const orgData = data.organizationInfo();
-            const saData = data.signingAuthorityInfo();
-            const groupData = data.groupNumbersInfo();
+        accessPage = new AccessAdminsPage();
+        if (data.hasJsonData === false) {
+            accessData = data.signingAuthorityInfo();
             data.setSeed();
         }
     });
 
-    it('01. should load the page without issue', () => {
-        aaPage.navigateTo();
-        expect(browser.getCurrentUrl()).toContain(AA_PAGE_URL);
-        expect(aaPage.formErrors().count()).toBe(0, 'should be no errors on page load');
+    fit('01. should load the page without issue', () => {
+        accessPage.navigateTo();
+        expect(browser.getCurrentUrl()).toContain(ACCESS_PAGE_URL);
+        expect(accessPage.formErrors().count()).toBe(0, 'should be no errors on page load');
     });
 
-    it('02. should let user to continue without filling out any fields', () => {
-        aaPage.navigateTo();
-        aaPage.clickButton('submit');
+    fit('02. should let user to continue without filling out any fields', () => {
+        accessPage.navigateTo();
+        accessPage.continue();
         expect(browser.getCurrentUrl()).toContain(USERS_PAGE_URL, 'should navigate to the Users page');
     });
 
     it('03. should let user to continue if all the required fields are filled out', () => {
-        aaPage.navigateTo();
-        aaPage.clickButton('btn btn-block');
-        // aaPage.fillInfo(aaData);
-        aaPage.fillInfo();
-        aaPage.scrollDown();
-        aaPage.selectValue('administeringFor', jsonData.administeringFor);
+        accessPage.navigateTo();
+        accessPage.clickButton('btn btn-block');
+        // accessPage.fillInfo(aaData);
+        accessPage.fillInfo();
+        accessPage.scrollDown();
+        accessPage.selectValue('administeringFor', jsonData.administeringFor);
         browser.sleep(5000);
-        aaPage.continue();
-        expect(aaPage.formErrors().count()).toBe(0, 'should be no errors after filling out');
+        accessPage.continue();
+        expect(accessPage.formErrors().count()).toBe(0, 'should be no errors after filling out');
         expect(browser.getCurrentUrl()).toContain(USERS_PAGE_URL, 'should navigate to the Users page');
     });
 
     it('04. should let user to continue when user clicks the x button', () => {
-        aaPage.navigateTo();
-        aaPage.clickButton('btn btn-block');
-        // aaPage.fillInfo(aaData);
-        aaPage.fillInfo();
-        aaPage.scrollDown();
-        aaPage.selectValue('administeringFor', jsonData.administeringFor);
-        aaPage.clickButton('btn delete');
-        aaPage.continue();
-        expect(aaPage.formErrors().count()).toBe(0, 'should be no errors after filling out');
+        accessPage.navigateTo();
+        accessPage.clickButton('btn btn-block');
+        // accessPage.fillInfo(aaData);
+        accessPage.fillInfo();
+        accessPage.scrollDown();
+        accessPage.selectValue('administeringFor', jsonData.administeringFor);
+        accessPage.clickButton('btn delete');
+        accessPage.continue();
+        expect(accessPage.formErrors().count()).toBe(0, 'should be no errors after filling out');
         expect(browser.getCurrentUrl()).toContain(USERS_PAGE_URL, 'should navigate to the Users page');
     });
 
     // Additional Tests
     it('05. should delete correct admin if user adds two admins and deletes one of them', () => {
-        aaData2 = data.signingAuthorityInfo();
-        aaPage.navigateTo();
-        aaPage.clickButton('btn btn-block');
-        // aaPage.fillInfo(aaData);
-        aaPage.fillInfo();
-        aaPage.scrollDown();
-        aaPage.selectValue('administeringFor', jsonData.administeringFor);
-        // aaPage.scrollUp();
-        aaPage.clickButton('btn btn-block');
-        // aaPage.fillInfo(aaData2);
-        aaPage.fillInfo();
-        aaPage.selectValue('administeringFor', jsonData.administeringFor);
-        aaPage.scrollDown();
-        aaPage.clickButton('btn delete'); // deletes the second admin created (latest one)
-        aaPage.continue();
-        expect(aaPage.formErrors().count()).toBe(0, 'should be no errors after filling out');
+        accessPage.navigateTo();
+        accessPage.clickButton('btn btn-block');
+        // accessPage.fillInfo(aaData);
+        accessPage.fillInfo();
+        accessPage.scrollDown();
+        accessPage.selectValue('administeringFor', jsonData.administeringFor);
+        // accessPage.scrollUp();
+        accessPage.clickButton('btn btn-block');
+        // accessPage.fillInfo(aaData2);
+        accessPage.fillInfo();
+        accessPage.selectValue('administeringFor', jsonData.administeringFor);
+        accessPage.scrollDown();
+        accessPage.clickButton('btn delete'); // deletes the second admin created (latest one)
+        accessPage.continue();
+        expect(accessPage.formErrors().count()).toBe(0, 'should be no errors after filling out');
         expect(browser.getCurrentUrl()).toContain(USERS_PAGE_URL, 'should navigate to the Users page');
     });
 
     it('06. should NOT be able to continue with an incomplete admin section even if another admin is complete', () => {
-        aaPage.navigateTo();
-        aaPage.clickButton('btn btn-block');
-        // aaPage.fillInfo(aaData);
-        aaPage.fillInfo();
-        aaPage.scrollDown();
-        aaPage.selectValue('administeringFor', jsonData.administeringFor);
-        aaPage.clickButton('btn btn-block');
-        aaPage.fillInfo();
-        aaPage.scrollDown();
-        aaPage.continue();
-        expect(browser.getCurrentUrl()).toContain(AA_PAGE_URL);
+        accessPage.navigateTo();
+        accessPage.clickButton('btn btn-block');
+        // accessPage.fillInfo(aaData);
+        accessPage.fillInfo();
+        accessPage.scrollDown();
+        accessPage.selectValue('administeringFor', jsonData.administeringFor);
+        accessPage.clickButton('btn btn-block');
+        accessPage.fillInfo();
+        accessPage.scrollDown();
+        accessPage.continue();
+        expect(browser.getCurrentUrl()).toContain(ACCESS_PAGE_URL);
     });
 
     // Test will fail since _autofill is still active in dev mode
     xit('07. should not be able to access "_autofill" dev only URL', () => {
-        // aaPage.navigateTo();
-        // expect(aaPage.getTextFromField()).toBe('', 'should be empty');
+        // accessPage.navigateTo();
+        // expect(accessPage.getTextFromField()).toBe('', 'should be empty');
     });
 
 });
