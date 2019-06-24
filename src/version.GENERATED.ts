14f2d540ea596eddf7cb277116bc8688dab5b025
Adam Coard <arcoard+github@gmail.com>
2019-06-24 13:56:41 -0400

    moh-common-lib@1.7.0 and implement CommonXIconButton

--- src/version.GENERATED.ts
+++ src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = 'e3f2996';
-export const buildTime = '6/24/2019 at 10:02:53 AM';
+export const gitCommit = '3aaa803';
+export const buildTime = '6/24/2019 at 10:44:17 AM';
 /** App version retrieved from package.json. */
 export const projectVersion = '0.0.0';
 /** Human readable message  */
-export const message = 'Canonical Version: e3f2996 - 6/24/2019 at 10:02:53 AM (v0.0.0) \nhttps://github.com/bcgov/moh-sitereg/commits/e3f2996';
+export const message = 'Canonical Version: 3aaa803 - 6/24/2019 at 10:44:17 AM (v0.0.0) \nhttps://github.com/bcgov/moh-sitereg/commits/3aaa803';
 /** If true, other values should be present. */
 export const success = true;
