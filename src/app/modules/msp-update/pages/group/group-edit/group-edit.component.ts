993aaf7e0978b4cc6a8f636e82fc540becd011a5
Faheem <faheemfactor@gmail.com>
2019-08-09 15:46:06 -0700

    code linted - group

--- src/app/modules/msp-update/pages/group/group-edit/group-edit.component.ts
+++ src/app/modules/msp-update/pages/group/group-edit/group-edit.component.ts
@@ -1,79 +1,89 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormArray, FormBuilder, Validators } from '@angular/forms';
 
-import { cUpdateValidators, validMultiFormControl, isValidOptionalField } from '../../../common/validators';
+import {
+    cUpdateValidators,
+    validMultiFormControl,
+    isValidOptionalField,
+} from '../../../common/validators';
 import { getEditJsonOfMspGroup } from '../shared/group-shared-json-map';
 import { environment } from 'src/environments/environment.prod';
 import { IDataForm, RandomObjects } from '../../../common/i-dataform';
 
-
 @Component({
-  selector: 'sitereg-update-group-edit',
-  templateUrl: './group-edit.component.html',
-  styleUrls: ['./group-edit.component.scss']
+    selector: 'sitereg-update-group-edit',
+    templateUrl: './group-edit.component.html',
+    styleUrls: ['./group-edit.component.scss'],
 })
 export class MspDirectUpdateGroupEditComponent implements OnInit, IDataForm {
+    private arrayFormPropertyName = 'arrayOfForms';
+    @Input() formState: FormGroup | null;
+    @Output() formArrayChanged: EventEmitter<
+        FormGroup | FormArray | null
+    > = new EventEmitter<FormGroup | null>();
+    parentForm: FormGroup;
+    validFormControl: (fg: FormGroup, name: string) => boolean;
+    radioBtnLabels = [
+        { label: 'No', value: 'N' },
+        { label: 'Yes', value: 'Y' },
+    ];
+    json: (formValues: any) => any;
 
-  private arrayFormPropertyName = 'arrayOfForms';
-  @Input() formState: FormGroup | null;
-  @Output() formArrayChanged: EventEmitter<FormGroup | FormArray | null> = new EventEmitter<FormGroup | null>();
-  parentForm: FormGroup;
-  validFormControl: (fg: FormGroup, name: string) => boolean;
-  radioBtnLabels = [{ label: 'No', value: 'N' }, { label: 'Yes', value: 'Y' }];
-  json: (formValues: any) => any;
-
-  constructor(private fb: FormBuilder) {
-    this.validFormControl = validMultiFormControl;
-    this.json = getEditJsonOfMspGroup;
-  }
-
-  ngOnInit() {
-    this.createOrUpdateForms();
-  }
-
-  private createOrUpdateForms() {
-    if (!this.formState) {
-      // console.log('create or update form');
-      this.parentForm = this.fb.group({
-        arrayOfForms: this.fb.array([])
-      });
-    } else {
-      this.parentForm = this.formState;
+    constructor(private fb: FormBuilder) {
+        this.validFormControl = validMultiFormControl;
+        this.json = getEditJsonOfMspGroup;
     }
-  }
 
-  private createArrayForm() {
-    const form = this.fb.group({
-      groupNo: ['', cUpdateValidators.group.groupNo],
-      thirdPartyAdmin: ['', Validators.required]
-    });
-    this.patchValue(form);
-    return form;
-  }
+    ngOnInit() {
+        this.createOrUpdateForms();
+    }
 
-  private removeForm(index: number) {
-    // console.log('Deleteing Form at ' + index);
-    this.getFormsArray.removeAt(index);
-    this.formArrayChanged.emit(this.parentForm);
-  }
+    private createOrUpdateForms() {
+        if (!this.formState) {
+            // console.log('create or update form');
+            this.parentForm = this.fb.group({
+                arrayOfForms: this.fb.array([]),
+            });
+        } else {
+            this.parentForm = this.formState;
+        }
+    }
 
+    private createArrayForm() {
+        const form = this.fb.group({
+            groupNo: ['', cUpdateValidators.group.groupNo],
+            thirdPartyAdmin: ['', Validators.required],
+        });
+        this.patchValue(form);
+        return form;
+    }
 
-  get getFormsArray(): FormArray | null {
-    return this.parentForm.get(this.arrayFormPropertyName) as FormArray | null;
-  }
+    private removeForm(index: number) {
+        // console.log('Deleteing Form at ' + index);
+        this.getFormsArray.removeAt(index);
+        this.formArrayChanged.emit(this.parentForm);
+    }
 
-  get getFormsCount(): number {
-    return this.getFormsArray ? this.getFormsArray.controls.length : 0;
-  }
+    get getFormsArray(): FormArray | null {
+        return this.parentForm.get(
+            this.arrayFormPropertyName
+        ) as FormArray | null;
+    }
 
-  public newForm() {
-    // console.log('Adding new Form');
-    this.getFormsArray.insert(0, this.createArrayForm());
-    this.formArrayChanged.emit(this.parentForm);
-  }
+    get getFormsCount(): number {
+        return this.getFormsArray ? this.getFormsArray.controls.length : 0;
+    }
 
-  patchValue(formGroup) {
-    if (!environment.useDummyData) return;
-    formGroup.patchValue(RandomObjects.getGroup((this.getFormsCount + 1).toString()));
-  }
+    public newForm() {
+        // console.log('Adding new Form');
+        this.getFormsArray.insert(0, this.createArrayForm());
+        this.formArrayChanged.emit(this.parentForm);
+    }
+
+    patchValue(formGroup) {
+        if (!environment.useDummyData) return;
+        formGroup.patchValue(
+            RandomObjects.getGroup((this.getFormsCount + 1).toString())
+        );
+    }
 }
