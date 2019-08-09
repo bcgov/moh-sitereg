75d0845319acc425484fc3be622e15ccab950224
Faheem <faheemfactor@gmail.com>
2019-08-09 12:58:09 -0700

    code linted - requestor

--- src/app/modules/msp-update/pages/requestor/requestor/requestor.component.ts
+++ src/app/modules/msp-update/pages/requestor/requestor/requestor.component.ts
@@ -1,24 +1,32 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import {
+    FormGroup,
+    FormBuilder,
+    Validators,
+    FormControl,
+} from '@angular/forms';
 import { ROUTES_UPDATE } from '../../../routing/routes.constants';
 import { Router } from '@angular/router';
 import { MspDirectUpdateProgressService } from '../../../services/progress.service';
 import { LoggerService } from '@shared/services/logger.service';
 import { GlobalConfigService } from '@shared/services/global-config.service';
 import { UpdateStateService } from '../../../services/update.state.service';
-import { cUpdateValidators, validMultiFormControl } from '../../../common/validators';
+import {
+    cUpdateValidators,
+    validMultiFormControl,
+} from '../../../common/validators';
 
 import { getJSONofRequestor } from '../shared/requestor-json-map';
 import { IDataForm, RandomObjects } from '../../../common/i-dataform';
 import { environment } from 'src/environments/environment.prod';
-
 
 @Component({
     selector: 'sitereg-msp-update-requestor',
     templateUrl: './requestor.component.html',
     styleUrls: ['./requestor.component.scss'],
 })
-export class MspDirectUpdateRequestorComponent implements OnInit, AfterViewInit, IDataForm {
+export class MspDirectUpdateRequestorComponent
+    implements OnInit, AfterViewInit, IDataForm {
     @ViewChild('consentModal') consentModal;
     validFormControl: (fg: FormGroup, name: string) => boolean;
     json: (formValues: any) => any;
@@ -49,10 +57,15 @@ export class MspDirectUpdateRequestorComponent implements OnInit, AfterViewInit,
     }
 
     createForm(): FormGroup {
-
         const form = this.fb.group({
-            organizationNumber: ['', cUpdateValidators.requestorInformation.organizationNumber],
-            emailAddress: ['', cUpdateValidators.requestorInformation.emailAddress]
+            organizationNumber: [
+                '',
+                cUpdateValidators.requestorInformation.organizationNumber,
+            ],
+            emailAddress: [
+                '',
+                cUpdateValidators.requestorInformation.emailAddress,
+            ],
         });
         this.patchValue(form);
         return form;
@@ -62,15 +75,12 @@ export class MspDirectUpdateRequestorComponent implements OnInit, AfterViewInit,
         // splunk-log
         this.loggerSvc.logNavigation(
             this.constructor.name,
-            `Valid Data - Continue button clicked. ${
-            this.globalConfigSvc.applicationId
-            }`
+            `Valid Data - Continue button clicked. ${this.globalConfigSvc.applicationId}`
         );
 
         this.progressService.setPageComplete();
         this.router.navigate([ROUTES_UPDATE.ORGANIZATION.fullpath]);
     }
-
 
     onAcceptCollectionNotice(accepted: boolean) {
         this.updateStateService.hasConsentedToInformationCollection = accepted;
@@ -82,6 +92,8 @@ export class MspDirectUpdateRequestorComponent implements OnInit, AfterViewInit,
 
     patchValue(formGroup) {
         if (!environment.useDummyData) return;
-        formGroup.patchValue(RandomObjects.getRequestor('RequestorInformation'));
+        formGroup.patchValue(
+            RandomObjects.getRequestor('RequestorInformation')
+        );
     }
 }
