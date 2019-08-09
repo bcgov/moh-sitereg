076ae572d3884d294ecebeed3a65c100c42ca9be
Faheem <faheemfactor@gmail.com>
2019-08-09 15:38:39 -0700

    code linted - user

--- src/app/modules/msp-update/pages/user/user-add/user-add.component.ts
+++ src/app/modules/msp-update/pages/user/user-add/user-add.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import {
-  cUpdateUserEnumeration, cUpdateUserValidator
+    cUpdateUserEnumeration,
+    cUpdateUserValidator,
 } from '../shared/user-shared';
 
 import {
-  validMultiFormControl, formControlValidity, matchFieldValidator,
+    validMultiFormControl,
+    formControlValidity,
+    matchFieldValidator,
 } from '../../../common/update-validators';
 
 import { getAddJsonOfMspUser } from '../shared/user-shared-json-map';
@@ -13,90 +16,109 @@ import { environment } from 'src/environments/environment.prod';
 import { IDataForm, RandomObjects } from '../../../common/i-dataform';
 
 @Component({
-  selector: 'sitereg-update-user-add',
-  templateUrl: './user-add.component.html',
-  styleUrls: ['./user-add.component.scss']
+    selector: 'sitereg-update-user-add',
+    templateUrl: './user-add.component.html',
+    styleUrls: ['./user-add.component.scss'],
 })
 export class MspDirectUpdateUserAddComponent implements OnInit, IDataForm {
+    @Input() showAdministeringMSPForQuestion = true; // needed in MSP updates only
+    private arrayFormPropertyName = 'arrayOfForms';
+    @Input() formState: FormGroup | null;
+    @Output() formArrayChanged: EventEmitter<
+        FormGroup | FormArray | null
+    > = new EventEmitter<FormGroup | null>();
+    parentForm: FormGroup;
+    validFormControl: (fg: FormGroup, name: string) => boolean;
+    formControlValidity: (
+        fg: FormGroup,
+        name: string
+    ) => { required: boolean; other: boolean };
 
-  @Input() showAdministeringMSPForQuestion = true; // needed in MSP updates only
-  private arrayFormPropertyName = 'arrayOfForms';
-  @Input() formState: FormGroup | null;
-  @Output() formArrayChanged: EventEmitter<FormGroup | FormArray | null> = new EventEmitter<FormGroup | null>();
-  parentForm: FormGroup;
-  validFormControl: (fg: FormGroup, name: string) => boolean;
-  formControlValidity: (fg: FormGroup, name: string) => { required: boolean; other: boolean };
+    userTitles = cUpdateUserEnumeration.userTitles;
+    administeringForOptions = cUpdateUserEnumeration.administeringFor.add;
+    json: (formValues: any) => any;
 
-  userTitles = cUpdateUserEnumeration.userTitles;
-  administeringForOptions = cUpdateUserEnumeration.administeringFor.add;
-  json: (formValues: any) => any;
-
-
-  constructor(private fb: FormBuilder) {
-    this.validFormControl = validMultiFormControl;
-    this.formControlValidity = formControlValidity;
-    this.json = getAddJsonOfMspUser;
-  }
-
-  ngOnInit() {
-    this.createOrUpdateForms();
-  }
-
-  private createOrUpdateForms() {
-    if (!this.formState) {
-      // console.log('create or update form');
-      this.parentForm = this.fb.group({
-        arrayOfForms: this.fb.array([])
-      });
-    } else {
-      this.parentForm = this.formState;
+    constructor(private fb: FormBuilder) {
+        this.validFormControl = validMultiFormControl;
+        this.formControlValidity = formControlValidity;
+        this.json = getAddJsonOfMspUser;
     }
-  }
 
-  private createArrayForm() {
-    const form =  this.fb.group({
-      userTitle: [null, cUpdateUserValidator.add.userTitle],
-      firstName: [null, cUpdateUserValidator.add.firstName],
-      initial: [null, cUpdateUserValidator.add.initial],
-      lastName: [null, cUpdateUserValidator.add.lastName],
-      jobTitle: [null, cUpdateUserValidator.add.jobTitle],
-      formGroupEmail: this.fb.group({
-        emailAddress: [null, cUpdateUserValidator.edit.emailAddress],
-        confirmEmail: [null, cUpdateUserValidator.edit.confirmEmail],
-      }, { validator: matchFieldValidator('confirmEmail', 'emailAddress') }),
-      phone: [null, cUpdateUserValidator.add.phone],
-      ext: [null, cUpdateUserValidator.add.ext],
-      fax: [null, cUpdateUserValidator.add.fax],
-      administeringFor: [null, cUpdateUserValidator.add.administeringFor],
-    });
-    this.patchValue(form);
-    return form;
-  }
+    ngOnInit() {
+        this.createOrUpdateForms();
+    }
 
-  private removeForm(index: number) {
-    // console.log('Deleteing Form at ' + index);
-    this.getFormsArray.removeAt(index);
-    this.formArrayChanged.emit(this.parentForm);
-  }
+    private createOrUpdateForms() {
+        if (!this.formState) {
+            // console.log('create or update form');
+            this.parentForm = this.fb.group({
+                arrayOfForms: this.fb.array([]),
+            });
+        } else {
+            this.parentForm = this.formState;
+        }
+    }
 
+    private createArrayForm() {
+        const form = this.fb.group({
+            userTitle: [null, cUpdateUserValidator.add.userTitle],
+            firstName: [null, cUpdateUserValidator.add.firstName],
+            initial: [null, cUpdateUserValidator.add.initial],
+            lastName: [null, cUpdateUserValidator.add.lastName],
+            jobTitle: [null, cUpdateUserValidator.add.jobTitle],
+            formGroupEmail: this.fb.group(
+                {
+                    emailAddress: [
+                        null,
+                        cUpdateUserValidator.edit.emailAddress,
+                    ],
+                    confirmEmail: [
+                        null,
+                        cUpdateUserValidator.edit.confirmEmail,
+                    ],
+                },
+                {
+                    validator: matchFieldValidator(
+                        'confirmEmail',
+                        'emailAddress'
+                    ),
+                }
+            ),
+            phone: [null, cUpdateUserValidator.add.phone],
+            ext: [null, cUpdateUserValidator.add.ext],
+            fax: [null, cUpdateUserValidator.add.fax],
+            administeringFor: [null, cUpdateUserValidator.add.administeringFor],
+        });
+        this.patchValue(form);
+        return form;
+    }
 
-  get getFormsArray(): FormArray | null {
-    return this.parentForm.get(this.arrayFormPropertyName) as FormArray | null;
-  }
+    private removeForm(index: number) {
+        // console.log('Deleteing Form at ' + index);
+        this.getFormsArray.removeAt(index);
+        this.formArrayChanged.emit(this.parentForm);
+    }
 
-  get getFormsCount(): number {
-    return this.getFormsArray ? this.getFormsArray.controls.length : 0;
-  }
+    get getFormsArray(): FormArray | null {
+        return this.parentForm.get(
+            this.arrayFormPropertyName
+        ) as FormArray | null;
+    }
 
-  public newForm() {
-    // console.log('Adding new Form');
-    this.getFormsArray.insert(0, this.createArrayForm());
-    this.formArrayChanged.emit(this.parentForm);
-  }
+    get getFormsCount(): number {
+        return this.getFormsArray ? this.getFormsArray.controls.length : 0;
+    }
 
-  patchValue(formGroup) {
-    if (!environment.useDummyData) return;
-    formGroup.patchValue(RandomObjects.getUser02((this.getFormsCount + 1).toString() + 'USR'));
-  }
+    public newForm() {
+        // console.log('Adding new Form');
+        this.getFormsArray.insert(0, this.createArrayForm());
+        this.formArrayChanged.emit(this.parentForm);
+    }
 
+    patchValue(formGroup) {
+        if (!environment.useDummyData) return;
+        formGroup.patchValue(
+            RandomObjects.getUser02((this.getFormsCount + 1).toString() + 'USR')
+        );
+    }
 }
