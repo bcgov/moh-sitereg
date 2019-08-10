e7013846fb1940868ea2ae54b686f5fb8d0e30a3
Faheem <faheemfactor@gmail.com>
2019-08-09 17:17:33 -0700

    code linted - routing

--- src/app/modules/msp-update/routing/routes.ts
+++ src/app/modules/msp-update/routing/routes.ts
@@ -30,48 +30,48 @@ let defaultRoutes: Routes = [
         path: ROUTES_UPDATE.ORGANIZATION.path,
         component: MspDirectUpdateOrganizationComponent,
         data: { title: ROUTES_UPDATE.ORGANIZATION.title },
-        canActivate: [RouteGuardService]
+        canActivate: [RouteGuardService],
     },
     {
         // path: 'signing-authority',
         path: ROUTES_UPDATE.SIGNING_AUTHORITY.path,
         component: MspDirectUpdateSigningAuthorityComponent,
         data: { title: ROUTES_UPDATE.SIGNING_AUTHORITY.title },
-        canActivate: [RouteGuardService]
+        canActivate: [RouteGuardService],
     },
     {
         // path: 'access-admins',
         path: ROUTES_UPDATE.ACCESS_ADMINS.path,
         component: MspDirectUpdateAccessAdministratorComponent,
         data: { title: ROUTES_UPDATE.ACCESS_ADMINS.title },
-        canActivate: [RouteGuardService]
+        canActivate: [RouteGuardService],
     },
     {
         // path: 'users',
         path: ROUTES_UPDATE.USERS.path,
         component: MspDirectUpdateUserComponent,
         data: { title: ROUTES_UPDATE.USERS.title },
-        canActivate: [RouteGuardService]
+        canActivate: [RouteGuardService],
     },
     {
         // path: 'group-numbers',
         path: ROUTES_UPDATE.GROUP_NUMBERS.path,
         component: MspDirectUpdateGroupComponent,
         data: { title: ROUTES_UPDATE.GROUP_NUMBERS.title },
-        canActivate: [RouteGuardService]
+        canActivate: [RouteGuardService],
     },
     {
         // path: 'Review',
         path: ROUTES_UPDATE.REVIEW.path,
         component: MspUpdateReviewComponent,
-        data: { title: ROUTES_UPDATE.REVIEW.title }
+        data: { title: ROUTES_UPDATE.REVIEW.title },
     },
     {
         // path: 'Submit',
         path: ROUTES_UPDATE.SUBMIT.path,
         component: MspDirectUpdateSubmitComponent,
         data: { title: ROUTES_UPDATE.SUBMIT.title },
-        canActivate: [RouteGuardService]
+        canActivate: [RouteGuardService],
     },
     {
         // path: 'confirmation',
@@ -83,13 +83,12 @@ let defaultRoutes: Routes = [
     // { path: '**', component: MspDirectUpdateIdentifyComponent },
 ];
 
-if (environment.bypassGuards || true ) {
+if (environment.bypassGuards || true) {
     // console.log('DEVELOPMENT ONLY - BYPASSING ROUTE GUARDS');
-    defaultRoutes = defaultRoutes.map(x => {
+    defaultRoutes = defaultRoutes.map((x) => {
         x.canActivate = [];
         return x;
     });
 }
-
 
 export const routes = defaultRoutes;
